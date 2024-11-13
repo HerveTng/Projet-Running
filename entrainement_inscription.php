2e6f8a86f2666ce253781497dd6c5a70329bf8a3
Maizze <matthieu.tevane@groupe-esigelec.org>
2024-11-13 10:32:51 +0100

    -1 pour le nombre max

--- entrainement_inscription.php
+++ entrainement_inscription.php
@@ -8,8 +8,22 @@ if (isset($_SESSION['entrainement_Id'], $_SESSION['user_mail'])) {
     $nom_course = $_SESSION['entrainement_Id'];
     $mail_inscrit = $_SESSION['user_mail'];
 
-    $statement = $db->prepare("INSERT INTO liste_inscrit (nom_Course, mail_inscrit) VALUES (:nom_course, :mail_inscrit)");
+    $statement0 = $db->prepare('SELECT nombre_Max FROM entrainement WHERE entrainement_Id = :entrainement_Id');
+    $statement1 = $db->prepare("INSERT INTO liste_inscrit (nom_Course, mail_inscrit) VALUES (:nom_course, :mail_inscrit)");
+    $statement2 = $db->prepare("UPDATE entrainement SET nombre_Max= :nombre WHERE entrainement_Id = :entrainement_Id ");
 
+    if ($statement0->execute([
+        'entrainement_Id' => $nom_course,
+    ]))
+
+    $result = $statement0->fetch(PDO::FETCH_ASSOC);
+    $nombre = $result['nombre_Max'] - 1;
+
+    if ($statement2->execute([
+        'nombre' => $nombre,
+        'entrainement_Id' => $nom_course,
+    ]))
+    
     if ($statement->execute([
         'nom_course' => $nom_course,
         'mail_inscrit' => $mail_inscrit,
